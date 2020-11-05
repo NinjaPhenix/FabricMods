c05e797061367bfd1e9864f35a85ddf2f0b81aa2
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2020-11-05 18:17:30 +0000

    Maybe update BER'd items

--- expandedstorage/src/main/java/ninjaphenix/expandedstorage/client/ExpandedStorageClient.java
+++ expandedstorage/src/main/java/ninjaphenix/expandedstorage/client/ExpandedStorageClient.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
@@ -15,12 +16,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Tuple;
+import net.minecraft.world.item.BlockItem;
 import ninjaphenix.chainmail.api.config.JanksonConfigParser;
 import ninjaphenix.expandedstorage.client.config.ContainerConfig;
 import ninjaphenix.expandedstorage.client.screen.PagedScreen;
@@ -31,6 +35,7 @@ import ninjaphenix.expandedstorage.common.Const;
 import ninjaphenix.expandedstorage.common.ExpandedStorage;
 import ninjaphenix.expandedstorage.common.ModContent;
 import ninjaphenix.expandedstorage.common.Registries;
+import ninjaphenix.expandedstorage.common.block.CursedChestBlock;
 import ninjaphenix.expandedstorage.common.block.entity.CursedChestBlockEntity;
 import ninjaphenix.expandedstorage.common.misc.CursedChestType;
 import org.apache.logging.log4j.MarkerManager;
@@ -41,6 +46,7 @@ public final class ExpandedStorageClient implements ClientModInitializer
 {
     @SuppressWarnings({"unused", "RedundantSuppression"})
     public static final ExpandedStorageClient INSTANCE = new ExpandedStorageClient();
+    private static final CursedChestBlockEntity CURSED_CHEST_RENDER_ENTITY = new CursedChestBlockEntity(BlockPos.ZERO, DIAMOND_CHEST.defaultBlockState());
     public static final ContainerConfig CONFIG = getConfigParser().load(ContainerConfig.class, ContainerConfig::new, getConfigPath(),
                                                                         new MarkerManager.Log4jMarker(Const.MOD_ID));
 
@@ -74,14 +80,13 @@ public final class ExpandedStorageClient implements ClientModInitializer
                         .map(data::getChestTexture).forEach(registry::register)));
 
         BlockEntityRendererRegistry.INSTANCE.register(ModContent.CHEST, (BlockEntityRendererProvider<CursedChestBlockEntity>) CursedChestBlockEntityRenderer::new);
-        // todo: BER item models
-        //ModContent.CHEST.validBlocks.forEach(block -> BuiltinItemRendererRegistry.INSTANCE.register(
-        //        block.asItem(), (itemStack, type, stack, vertexConsumers, light, overlay) ->
-        //        {
-        //            CursedChestBlock renderBlock = (CursedChestBlock) ((BlockItem) itemStack.getItem()).getBlock();
-        //            CURSED_CHEST_RENDER_ENTITY.setBlock(renderBlock.TIER_ID);
-        //            BlockEntityRenderDispatcher.instance.renderItem(CURSED_CHEST_RENDER_ENTITY, stack, vertexConsumers, light, overlay);
-        //        }));
+        ModContent.CHEST.validBlocks.forEach(block -> BuiltinItemRendererRegistry.INSTANCE.register(
+                block.asItem(), (itemStack, type, stack, vertexConsumers, light, overlay) ->
+                {
+                    CursedChestBlock renderBlock = (CursedChestBlock) ((BlockItem) itemStack.getItem()).getBlock();
+                    CURSED_CHEST_RENDER_ENTITY.setBlock(renderBlock.TIER_ID);
+                    Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(CURSED_CHEST_RENDER_ENTITY, stack, vertexConsumers, light, overlay);
+                }));
         ScreenRegistry.register(SCROLLABLE_HANDLER_TYPE, ScrollableScreen::new);
         ScreenRegistry.register(PAGED_HANDLER_TYPE, PagedScreen::new);
         ScreenRegistry.register(SINGLE_HANDLER_TYPE, SingleScreen::new);
